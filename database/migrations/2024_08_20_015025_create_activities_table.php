d5e9ecbf978ff0c7ba0b1f7fef992033ae950161
RamaTaufik <ramata2447@gmail.com>
2024-08-20 14:14:51 +0700

    Adjust&Add: Migration files

---
+++ database/migrations/2024_08_20_015025_create_activities_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('name')->default('Judul Artikel');
+            $table->text('description')->nullable();
+            $table->boolean('is_draft');
+            $table->foreignId('admin_id')->constrained(table: 'users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
