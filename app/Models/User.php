182fb1a6fc31dec8252359ac082918ba0553805f
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-08-25 16:39:12 +0700

    add canAccessPanel on User.php model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -11,6 +12,11 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(Panel $panel)
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
