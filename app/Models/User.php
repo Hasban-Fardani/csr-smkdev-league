d31d43f979c98ff9e4bf1f10de4cd46a6bceac21
Hasban Fardani <hasbanfardani77@gmail.com>
2024-08-28 20:30:06 +0700

    Update User.php
    fix syntax error

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements FilamentUser
-
+{
     use HasFactory, Notifiable;
 
     public function canAccessPanel(Panel $panel): bool
